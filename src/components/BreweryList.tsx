dfe3cb4e763d2d17585e9d85f1728b8f9a911229
Raymondg3131 <114846526+Raymondg3131@users.noreply.github.com>
2025-09-29 23:32:21 -0400

    styling changes

--- src/components/BreweryList.tsx
+++ src/components/BreweryList.tsx
@@ -22,11 +22,11 @@ const SingleBreweriesDiv = styled.div<{ type: string}>`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    max-width: 30%;
-    padding: 2%;
-    margin: 1%;
-    background-color: ${(props) => (props.type === "micro" ? '#FFD700' : (props.type === "closed" ? '#800000' : '#A9A9A9'))};
-    border: 3px solid #333;
+    max-width: 14%;
+    padding: 1.8%;
+    margin: 0.8%;
+    background-color: ${(props) => (props.type === "micro" ? '#0064ff' : (props.type === "closed" ? '#800000' : '#A9A9A9'))};
+    border: 2px solid #333;
     font-family: Arial, sans-serif;
     text-align: center;
     color: white;
@@ -47,46 +47,48 @@ const SingleBreweriesDiv = styled.div<{ type: string}>`
 `;
 
 const BreweryName = styled.h1`
-    margin: 2% 0;
-    font-size: 4vw;
+    margin: 1.5% 0;
+    font-size: 2.2vw;
+    padding-bottom: 4%;
     
     @media (max-width: 768px) {
-        font-size: 3.5vw;
+        font-size: 1.9vw;
         margin: 1.5% 0;
     }
     
     @media (max-width: 480px) {
-        font-size: 3vw;
+        font-size: 2vw;
         margin: 1% 0;
     }
 `;
 
 const BreweryType = styled.h2`
-    margin: 1% 0;
-    font-size: 3.2vw;
+    margin: 0.8% 0;
+    font-size: 1.6vw;
+    padding-bottom: 4%;
     
     @media (max-width: 768px) {
-        font-size: 3vw;
+        font-size: 2vw;
         margin: 0.8% 0;
     }
     
     @media (max-width: 480px) {
-        font-size: 2.8vw;
+        font-size: 1.8vw;
         margin: 0.5% 0;
     }
 `;
 
 const BreweryLocation = styled.p`
-    margin: 1% 0;
-    font-size: 2.8vw;
+    margin: 0.8% 0;
+    font-size: 2.2vw;
     
     @media (max-width: 768px) {
-        font-size: 2.5vw;
+        font-size: 1.9vw;
         margin: 0.8% 0;
     }
     
     @media (max-width: 480px) {
-        font-size: 2.2vw;
+        font-size: 2vw;
         margin: 0.5% 0;
     }
 `;
@@ -98,7 +100,7 @@ export default function BreweryList(props : {data: Brewery[]}){
                 props.data.map((brew: Brewery) =>
                     <SingleBreweriesDiv key={brew.id} type={brew.brewery_type}>
                         <BreweryName>{brew.name}</BreweryName>
-                        <BreweryType>{brew.brewery_type}</BreweryType>
+                        <BreweryType>({brew.brewery_type})</BreweryType>
                         <BreweryLocation>{brew.city}, {brew.state}</BreweryLocation>
                     </SingleBreweriesDiv>
                 )
